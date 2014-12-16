2e6b641c7dd1f206e708d5e5c74c8fd383854220
Tate Bosler <tatebosler@gmail.com>
2014-12-16 00:12:58 -0600

    Added basic article saving
    As can be seen in test/models/article_test.rb, I added some basic
    validation to ensure that all of the fields for Article are filled in.
    The controller has been updated to save created records to the
    database. The view has been updated to properly redirect, run “rake
    routes” to see what articles_path does.

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save article with missing title" do
+    article = Article.new("author" => "Scot Bot", "content" => "I like turtles")
+    assert_not article.save, "An untitled article was saved."
+  end
+  
+  test "should not save article with missing author" do
+    article = Article.new("title" => "Turtles are AWESOME", "content" => "I like turtles")
+    assert_not article.save, "An authorless article was saved."
+  end
+  
+  test "should not save article with missing content" do
+    article = Article.new("title" => "I like trains", "author" => "Daily asdfnews")
+    assert_not article.save, "An article was saved without content."
+  end
 end
